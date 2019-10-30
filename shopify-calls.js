428ede8d04cc60e090d6d44fdb8de3a044138b9c
Michael Peterson <WebDev@ctoms.ca>
2019-10-30 11:26:39 -0600

    setup initial promise framework

--- shopify-calls.js
+++ shopify-calls.js
@@ -1,5 +1,5 @@
 /** 
- * k-p.js
+ * shopify-calls.js
  * @NApiVersion 2.x
  * @NModuleScope Public 
  */
@@ -8,13 +8,19 @@
 define ([] , 
 
     function () {    
-        function getAllProducts (options) { 
-            var key = options.keyData.key;
-            var pass = options.keyData.pass;
-            var url = options.keyData.url;
-            var data = [];
-
-            return data;
+        function getAllProducts (options,productData) {
+            var promise = new Promise(function(resolve,reject){
+                if(!productData){
+                    productData = [];
+                }
+                var key = options.keyData.key;
+                var pass = options.keyData.pass;
+                var url = options.keyData.url;
+                
+                resolve(productData);
+            });
+            
+            return promise;
         }
 
     return {
