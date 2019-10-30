ec61e0a5d22d24c27e46fb442ca52fb829de7fb2
Michael Peterson <WebDev@ctoms.ca>
2019-10-30 10:59:04 -0600

    initial commit

---
+++ shopify-calls.js
@@ -0,0 +1,24 @@
+/** 
+ * k-p.js
+ * @NApiVersion 2.x
+ * @NModuleScope Public 
+ */
+
+
+define ([] , 
+
+    function () {    
+        function getAllProducts (options) { 
+            var key = options.keyData.key;
+            var pass = options.keyData.pass;
+            var url = options.keyData.url;
+            var data = [];
+
+            return data;
+        }
+
+    return {
+        getAllProducts: getAllProducts
+    }
+    
+});
